5dd93bb0e33393df7b3f782dabc2b8e3a992764c
saitam.m@gmail.com <saitam.m@gmail.com@38e6b009-f655-846b-0a4a-e2a8162a774d>
2012-05-17 23:32:11 +0000

    changes: settings,urls. Uploading: templates, views.

--- salary_calculator_app/models.py
+++ salary_calculator_app/models.py
@@ -6,9 +6,9 @@ from salary_calculator_app.validators import *
 
 
 class Cargo(models.Model):
-	lu 				= models.CharField(u'Código LU', max_length=2, unique=True, validators=[validate_isdigit])
+	lu 		= models.CharField(u'Código LU', max_length=2, unique=True, validators=[validate_isdigit])
 	pampa 		= models.CharField(u'Código PAMPA', max_length=3, unique=True, validators=[validate_isdigit])
-	tipo 			= models.CharField(u'Tipo de Cargo', max_length=50, unique=True)
+	tipo 		= models.CharField(u'Tipo de Cargo', max_length=50, unique=True)
 	basico 		= models.FloatField(u'Sueldo Básico')
 	garantia 	= models.FloatField(u'Garantía Salarial', blank=True)
 
@@ -21,15 +21,15 @@ class CargoPreUniv(Cargo):
 
 
 class Antiguedad(models.Model):
-	anio 				= models.SmallIntegerField(u'Años de Antiguedad', unique=True)
+	anio 		= models.SmallIntegerField(u'Años de Antiguedad', unique=True)
 	porcentaje 	= models.FloatField(u'Porcentaje')
 
 	def __unicode__(self):
-		return self.anio + ":" + self.porcentaje
+		return self.anio + ": " + self.porcentaje
 
 
 class Aumento(models.Model):
-	fecha			= models.DateField(u'Fecha', unique=True)
+	fecha		= models.DateField(u'Fecha', unique=True)
 	porcentaje 	= models.FloatField(u'Porcentaje')
 
 	def __unicode__(self):
@@ -38,7 +38,7 @@ class Aumento(models.Model):
 
 class Retencion(models.Model):
 	codigo 	= models.CharField(u'Código', max_length=3, validators=[validate_isdigit])
-	nombre = models.CharField(u'Nombre', max_length=50)
+	nombre  = models.CharField(u'Nombre', max_length=50)
 	porcentage = models.FloatField(u'Porcentaje')
 
 	def __unicode__(self):
--- salary_calculator_app/views.py
+++ salary_calculator_app/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import Context, Template
+
+from models import *
+
+def calculate(request):
+	req = request.POST.get()
+	
+
+	return render_to_response('salary_calculated.html', context)
+
+	

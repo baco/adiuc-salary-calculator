4cb8215014260c75de71305c8b8c84da63ff586a
david.racca@gmail.com <david.racca@gmail.com@38e6b009-f655-846b-0a4a-e2a8162a774d>
2012-05-22 18:36:50 +0000

    Little changes made in Cargo model. Changed Cargo Form.

--- salary_calculator_app/forms.py
+++ salary_calculator_app/forms.py
@@ -1,23 +1,31 @@
+# -*- coding:utf-8 -*-
+
 from django import forms
+from models import *
 
-year_choices = (
-('0','0'),
-('1','1'),
-('2','2'),
-('5','5'),
-('7','7'),
-('10','10'),
-('12','12'),
-('15','15'),
-('17','17'),
-('20','20'),
-('22','22'),
-('24','24'),
+# Una forma pulenta de hacer esto es usando los 'ModelChoiceField'
 
-)
+#year_choices = (
+#('0','0'),
+#('1','1'),
+#('2','2'),
+#('5','5'),
+#('7','7'),
+#('10','10'),
+#('12','12'),
+#('15','15'),
+#('17','17'),
+#('20','20'),
+#('22','22'),
+#('24','24'),
+#)
 
-class CargoForm(forms.Form):
-	tipo 		= forms.CharField(max_length=50)
-	antiguedad 	= forms.ChoiceField(choices=year_choices)
-	master 		= forms.BooleanField(required=False)
-	doctorado 	= forms.BooleanField(required=False)
+class CargoUnivForm(forms.Form):
+    """Formulario de calculo de salario docente para docentes universitarios."""
+
+    cargo = forms.ModelChoiceField(label=u'Cargo', queryset=CargoUniv.objects.all(), empty_label=None,
+        help_text=u'Ingrese el nombre del cargo.')
+    antiguedad = forms.ModelChoiceField(label=u'Antigüedad', queryset=AntiguedadUniv.objects.all(), empty_label=None,
+        help_text=u'Ingrese su antigüedad para el cargo.')
+    master = forms.BooleanField(label=u'¿Posee título de maestría?', required=False)
+    doctorado = forms.BooleanField(label=u'¿Posee título de doctorado?', required=False)

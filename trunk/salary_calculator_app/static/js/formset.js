a6c3764259ed75b7efbaeaeff8ccc8dd47b57d4b
david.racca@gmail.com <david.racca@gmail.com@38e6b009-f655-846b-0a4a-e2a8162a774d>
2012-05-24 22:56:39 +0000

    Merged views.py and added code to get data from the formset.

--- trunk/salary_calculator_app/static/js/formset.js
+++ trunk/salary_calculator_app/static/js/formset.js
@@ -1,17 +1,19 @@
-function show_new_form_HTML(father_id, formhtml, totalforms_id, maxforms_id, addbeforeobj_id)
+function show_new_form_HTML(father_id, divAttrs, divContent, formsetPrefix, addbeforeobj_id)
 {
 	var fatherObj = document.getElementById(father_id);
 	var newdiv = document.createElement("div");
-	var totalformsObj = document.getElementById(totalforms_id);
-    var maxformsObj = document.getElementById(maxforms_id);
+	var totalformsObj = document.getElementById("id_" + formsetPrefix + "-TOTAL_FORMS");
+    var maxformsObj = document.getElementById("id_" + formsetPrefix + "-MAX_NUM_FORMS");
     var addbeforeObj = document.getElementById(addbeforeobj_id);
 
 	var totalforms = parseInt(totalformsObj.value);
     var maxforms = parseInt(maxformsObj.value);
 
     if (totalforms < maxforms) {
-	    formhtml = formhtml.replace(/__prefix__/g, totalformsObj.value);
-	    newdiv.innerHTML = formhtml;
+	    divContent = divContent.replace(/__prefix__/g, totalformsObj.value);
+	    newdiv.innerHTML = divContent;
+        for (var attr in divAttrs)
+            newdiv.setAttribute(attr, divAttrs[attr]);
 	    $(newdiv).hide();
 	    totalformsObj.value = totalforms + 1 + ""; // Asi lo convierto a String.
 	    fatherObj.insertBefore(newdiv, addbeforeObj);
@@ -19,9 +21,9 @@ function show_new_form_HTML(father_id, formhtml, totalforms_id, maxforms_id, add
     }
 }
 
-function hide_father(node, totalforms_id) {
-	//var delcheckObj = node.parentElement.parentElement.lastElementChild.children[0];
-    var totalformsObj = document.getElementById(totalforms_id);
+function hide_father(node, formsetPrefix)
+{
+    var totalformsObj = document.getElementById("id_" + formsetPrefix + "-TOTAL_FORMS");
     var totalforms = parseInt(totalformsObj.value);
     totalformsObj.value = totalforms - 1 + "";
 	$(node.parentElement.parentElement).slideUp("slow");

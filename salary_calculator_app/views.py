2ffbad79e2e20ca009338dce7b508d8b9feda851
saitam.m@gmail.com <saitam.m@gmail.com@38e6b009-f655-846b-0a4a-e2a8162a774d>
2012-05-26 00:30:26 +0000

    changes in view. Changes in salary_calculate.html: rendering results

--- salary_calculator_app/views.py
+++ salary_calculator_app/views.py
@@ -12,37 +12,38 @@ import models
 #import pdb
 
 def calculate(request):
-    #error = 1 : error einvformset
-    #error = 2 : error formularios invalidos
-
 #    pdb.set_trace()
-
-    context = {}
 
     # CargoUnivFormSet: Permite que aparezcan multiples formularios identicos.
     CargoUnivFormSet = formset_factory(CargoUnivForm, extra=1, max_num=5)
 
+    context = {}
     if request.method == 'POST':
 
         # Sacamos la info del POST y bindeamos los forms.
         univformset = CargoUnivFormSet(request.POST, prefix='univcargo')
         mform = MesForm(request.POST)
 
-        #if univformset.is_valid():
-         #   univformset = CargoUnivForm(request.POST)
-          #  context.update({'univformset':univformset, 'mform':mform})
-        #else:
-          #  error = 1
-           # print error
-            #context['error'] = error
-            #return render_to_response('calculate.html', context)
-
         if univformset.is_valid() and mform.is_valid():
 
             aumento_obj = mform.cleaned_data['aumento']
 
-            # Itero sobre todos los cargos.
+            #guardo en esta lista un diccionario para cada formulario procesado
+            #en cada una de estas, los resultados para renderizar luego.
+            lista_res = list()
             for univform in univformset:
+                lista_res.append(dict())
+
+            # Itero sobre todos los cargos.
+            i = 0
+            total_bruto = 0.0
+            total_neto = 0.0 
+
+            for univform in univformset:                
+                form_res = lista_res[i] #i-esimo diccionario de la lista. 
+                                        #para guardar los res del cargo i esimo                
+                salario_bruto = 0
+                salario_neto = 0
 
                 cargo_obj = univform.cleaned_data['cargo']
                 has_doctorado = univform.cleaned_data['doctorado']
@@ -53,6 +54,8 @@ def calculate(request):
                 anios_antiguedad = antiguedad_obj.anio
                 dedicacion = cargo_obj.dedicacion
                 tipo_cargo = cargo_obj.tipo
+                dedicacion = cargo_obj.dedicacion
+
                 basico_unc = cargo_obj.basico_unc
                 basico_nac = cargo_obj.basico_nac
 
@@ -61,12 +64,17 @@ def calculate(request):
                 anio = aumento_obj.anio
 
                 ldescuentos = []      #lista de cosas a descontar, retencioens, etc.
-                aumento2003   = 0
-                descuentos    = 0.19  #calcularlo basadose en lista_descuentos
-            
-                context.update({'ldescuentos':ldescuentos,'aumento2003':aumento2003,'descuentos':descuentos})
+                aumento2003 = 0
+                descuentos  = 0.19  #calcularlo basadose en lista_descuentos
 
-                # ej: Sueldo*aumento_posg = sueldo_resultante
+                tipo_cargo = str(tipo_cargo) + " " + str(dedicacion)
+                form_res.update({
+                                 'Tipo de Cargo':tipo_cargo,
+                                 'Aaumento desde 2003':aumento2003,
+                                 'Total Descuentos':descuentos,
+                                 'ldescuentos':ldescuentos                             
+                                })
+
                 if has_doctorado:
                     aumento_posg = 1.15
                 elif has_master:
@@ -79,23 +87,27 @@ def calculate(request):
                 neto_sep11 = neto_basico_sep11 * antiguedad
             
                 if mes == "SEP" and anio == "2011":
-                    
-                    context['bruto'] = bruto_sep11
-                    context['neto'] = neto_sep11
+                    salario_bruto = bruto_sep11
+                    salario_neto = neto_sep11
 
                 if mes == "MAR" or mes == "JUN" or (mes == "SEP" and anio == "2012"):
                     acum_mensual = basico_nac * aumento
                     salario_basico = basico_unc + acum_mensual + aumento2003
-                    salario_bruto = salario_basico * antiguedad
-                    
+                   
+                    salario_bruto = salario_basico * antiguedad                    
                     salario_bruto = salario_bruto * aumento_posg
                 
                     salario_neto = neto_sep11 + (neto_sep11 * aumento)
 
-                    context['bruto'] = salario_bruto
-                    context['neto'] = salario_neto
+                form_res.update({'Sueldo Bruto':salario_bruto,'Sueldo Neto':salario_neto})                    
+                total_bruto += salario_bruto
+                total_neto += salario_neto
+                i = i+1
 
-                    context['mes'] = mes
+            context['total_bruto']=total_bruto
+            context['total_neto'] = total_neto
+            context['lista_res'] = lista_res
+            print context
             return render_to_response('salary_calculated.html', context)
 
     else:
